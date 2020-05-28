18916e7210b855f8325ddf4e8bf6db7ab32abff0
Voinov Nikita <nikita-nikita2012@mail.ru>
2020-05-28 03:44:28 +0300

    firts commit extended syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,42 @@
+"use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+    let x ='';
+    let d = b**2 - 4 * a * c;
+    if (d < 0) { 
+        x=[];
+      console.log(x);
+   } else if (d == 0) {
+       x = [(-b + Math.sqrt(d)) / (2*a)];
+       console.log(x);
+   } else if (d > 0) {
+       x = [(-b + Math.sqrt(d)) / (2*a), (-b - Math.sqrt(d)) / (2*a)];
+       console.log(x);
+   };
+  
+     return x;
+};
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let averageMark = 0;
+    if (marks.length > 5) {
+        marks.splice(5);
+        averageMark = 'Производится расчет только из 5 оценок!';
+        return averageMark;
+    } else if (marks.length === 0) {
+        return 0;
+    }
+    for ( let i = 0; i < marks.length; i++) {
+        averageMark += marks[i];
+    }
+    return averageMark / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let result = new Date().getFullYear(); 
+    if (result - dateOfBirthday.getFullYear() > 18) {
+        return `Не желаете ли олд-фэшн ${name}`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    };
+    
 }
