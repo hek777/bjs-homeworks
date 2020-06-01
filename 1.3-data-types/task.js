868f1ee081e9cc10d44edaaa05c2d53ff258de4b
Voinov Nikita <nikita-nikita2012@mail.ru>
2020-06-02 02:01:39 +0300

    first 1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,47 @@
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    'use strict'
+    if (isNaN(Number(percent))) {
+        return 'Укаите "Процентную ставку" по ипотеке.';
+    }
+    if (isNaN(Number(contribution))) {
+        return 'Укажите "Начальный взнос".';
+    }
+    if (isNaN(Number(amount))) {
+        return 'Не указана "Общая стоимость" желаемого кредита.';
+    }
+    if (isNaN(date)) {
+        return 'Пожалуйста, укажите "Срок дейтсвия ипотеки"';
+    }
+    if (new Date(date).getFullYear() < new Date().getFullYear() ) {
+        return 'Выбрана не верная дата ипотеки.';
+    }
+    
+    date = {
+        'month' : new Date(date).getMonth() + 1,
+        'year' : new Date(date).getFullYear(),
+    }
+
+    let currentDate = {
+        'month' : new Date().getMonth() + 1,
+        'year' : new Date().getFullYear(),
+    };
+    let dateEnd = (12 * (date.year - currentDate.year)) - (currentDate.month - date.month);
+    let creditBody = amount - contribution;
+    let moneyInMounth = creditBody * (((1 / 12) * (percent / 100)) + ((1 / 12) * (percent / 100)) / ((( 1 + (( 1 /12) * (percent/100)) )**(dateEnd) ) - 1 ));
+    let totalAmount = (moneyInMounth * dateEnd).toFixed(2);
+        totalAmount = Number(totalAmount);
+    
+    if (isNaN(totalAmount)) {
+        console.log((date.getFullYear() - new Date().getFullYear()) * 12);
+        return 'Выбрана не верная дата ипотеки.';
+    }
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name == '' || name == null || name == undefined) {
+        return `Привет, мир! Меня зовут Аноним`
+    }
+     return `Привет, мир! Меня зовут ${name}`;
 }
